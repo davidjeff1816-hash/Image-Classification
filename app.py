4a033d23c4ebc6627f80677c2782418cbf417479
davidjeff1816-hash <davidjeff1816@gmail.com>
2025-12-16 19:17:44 +0530

    Update face recognition model to use TFLite

--- app.py
+++ app.py
@@ -1,58 +1,75 @@
 import streamlit as st
 import cv2
 import numpy as np
-import tensorflow as tf
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import json
 import os
 import gdown
+import tensorflow as tf
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
-# -------------------------------
-# Streamlit page config
-# -------------------------------
+# ------------------------------------
+# Streamlit Page Config
+# ------------------------------------
 st.set_page_config(page_title="Team Face Recognition", layout="centered")
 st.title("👤 Team Face Recognition (Live Webcam)")
 
-# -------------------------------
-# Download model from Google Drive if not present
-# -------------------------------
-MODEL_PATH = "face_model.h5"
-MODEL_URL = "https://drive.google.com/uc?id=116t-OB2Dc5w78aBwoWkfgj0sEOi0Ro5Q"
+# ------------------------------------
+# Download TFLite model from Google Drive
+# ------------------------------------
+MODEL_PATH = "face_model.tflite"
+MODEL_URL = "https://drive.google.com/uc?id=1Ce-wB10lyAIEQ5Ht-oDIQEdLEEeBvPJv"
 
 if not os.path.exists(MODEL_PATH):
-    st.info("Downloading model... Please wait ⏳")
+    st.info("Downloading face recognition model… ⏳")
     gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
 
-# -------------------------------
-# Load model
-# -------------------------------
-model = tf.keras.models.load_model(MODEL_PATH)
+# ------------------------------------
+# Load TFLite model
+# ------------------------------------
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+interpreter.allocate_tensors()
 
-# -------------------------------
-# Load labels
-# -------------------------------
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# ------------------------------------
+# Load class labels
+# ------------------------------------
 with open("labels.json", "r") as f:
     class_indices = json.load(f)
 
 labels = list(class_indices.keys())
 
-# -------------------------------
-# Load Haar Cascade for face detection
-# -------------------------------
+# ------------------------------------
+# Load Haar Cascade Face Detector
+# ------------------------------------
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
-# -------------------------------
-# Webcam video processor
-# -------------------------------
+# ------------------------------------
+# Prediction function
+# ------------------------------------
+def predict_face(face_img):
+    interpreter.set_tensor(
+        input_details[0]['index'],
+        face_img.astype(np.float32)
+    )
+    interpreter.invoke()
+    return interpreter.get_tensor(output_details[0]['index'])
+
+# ------------------------------------
+# Webcam Video Processor
+# ------------------------------------
 class VideoProcessor(VideoProcessorBase):
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         faces = face_cascade.detectMultiScale(
-            gray, scaleFactor=1.3, minNeighbors=5
+            gray,
+            scaleFactor=1.3,
+            minNeighbors=5
         )
 
         for (x, y, w, h) in faces:
@@ -61,13 +78,13 @@ class VideoProcessor(VideoProcessorBase):
             face = face / 255.0
             face = np.expand_dims(face, axis=0)
 
-            prediction = model.predict(face, verbose=0)
-            predicted_label = labels[np.argmax(prediction)]
+            prediction = predict_face(face)
+            label = labels[np.argmax(prediction)]
 
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.putText(
                 img,
-                predicted_label,
+                label,
                 (x, y - 10),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 0.9,
@@ -77,9 +94,9 @@ class VideoProcessor(VideoProcessorBase):
 
         return img
 
-# -------------------------------
-# Start webcam
-# -------------------------------
+# ------------------------------------
+# Start Webcam Stream
+# ------------------------------------
 webrtc_streamer(
     key="face-recognition",
     video_processor_factory=VideoProcessor,
